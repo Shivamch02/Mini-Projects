3d7ea02b74bc4601d0b4ba1d7aca24287604df29
Shivam Chaurasiya <88436482+Shivamch02@users.noreply.github.com>
2023-09-14 09:15:10 +0530

    Add files via upload

---
+++ To Do App/app.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
+
+btn.addEventListener('click',function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement('button');
+    delBtn.innerText = "delete";
+    delBtn.classList.add('delete');
+
+    ul.appendChild(item);
+    item.appendChild(delBtn);
+    inp.value = "";
+})
+
+ul.addEventListener('click',function(event){
+    
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log('deleted');
+    }
+})
+
+// let delBtns = document.querySelectorAll('.delete');
+
+// for(delBtn of delBtns){
+//     delBtn.addEventListener('click', function(){
+//         let par = this.parentElement; 
+//         par.remove();
+//     })
+// }
